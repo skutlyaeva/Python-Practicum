fac0aabf12052b73f9a2f8101631c6b0b569db96
Кутляева Софья <skutlyaeva@gmail.com>
2024-10-01 21:48:46 +0300

    Delete 2.3. Циклы directory

--- 2.3. Циклы/2.3_13.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-m = input()
-for i in range(n - 1):
-    m = min(m, input())
-print(m)
--- 2.3. Циклы/2.3_2.py
+++
@@ -1,7 +0,0 @@
-x = input()
-k = 0
-while x != 'Приехали!':
-    if 'зайка' in x:
-        k = k + 1
-    x = input()
-print(k)
