9e85caa354d4b8d8d44f4acc1250e052a76bc2db
Кутляева Софья <skutlyaeva@gmail.com>
2024-10-01 21:58:25 +0300

    Delete 2.2. Условный оператор directory

--- 2.2. Условный оператор/2.2_15.py
+++
@@ -1,8 +0,0 @@
-a = input()
-b = input()
-m = [int(a[0]), int(a[1]), int(b[0]), int(b[1])]
-mx = max(m)
-mn = min(m)
-m.remove(max(m))
-m.remove(min(m))
-print(int(str(mx) + str(sum(m) % 10) + str(mn)))
--- 2.2. Условный оператор/2.2_7.py
+++
@@ -1,6 +0,0 @@
-g = int(input())
-g = str(g)
-if (g[3] + g[2] + g[1] + g[0]) == g:
-    print("YES")
-else:
-    print("NO")
