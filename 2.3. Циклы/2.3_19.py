05fcbe7c856a825a729a201403df57d8bfe986db
skutlyaeva <skutlyaeva@gmail.com>
2024-10-03 20:03:44 +0300

    1

--- 2.3. Циклы/2.3_19.py
+++ 2.3. Циклы/2.3_19.py
@@ -1,11 +1,10 @@
-b = 500
-a = b // 2
-print(b)
+a, b = 1, 1000
+print((a + b) // 2)
 while (x := input()) != 'Угадал!':
     if x == 'Меньше':
-        b = b - a
-    if x == 'Больше':
-        b = b + a
-    if a >= 2:
-        a = (a + 1) // 2
-    print(b)
+        b = (a + b) // 2
+        print((a + b) // 2)
+    elif x == 'Больше':
+        a = (a + b) // 2
+        print((a + b) // 2)
+print('=' * 35)
--- 2.3. Циклы/2.3_20.py
+++ 2.3. Циклы/2.3_20.py
@@ -1,12 +1,18 @@
-r = -1
-H = 1
-for i in range(int(input())):
+q = int(input())
+H = 0
+o = 0
+err = False
+for i in range(q):
     b = int(input())
-    h, r, m = b % 256, (b // 256) % 256, b // 256 ** 2
-    t = ((m + r + H) * 37) % 256
-    if t != h or h > 99:
-        result = i
-        break
-    hn1 = h
-print(result)
-
+    h = b % 256
+    r = (b // 256) % 256
+    m = b // 256 ** 2
+    if (37 * (m + r + H)) % 256 != h or (37 * (m + r + H)) % 256 >= 100:
+        if err is False:
+            o = i
+            err = True
+    H = h
+if err == False:
+    print(-1)
+else:
+    print(o)
