93fcd3f86bbb1d54007a6527133f819fa4651f7b
Кутляева Софья <skutlyaeva@gmail.com>
2024-10-24 00:00:50 +0300

    Delete 2.5. КР «Базовые конструкции Python» directory

--- 2.5. КР «Базовые конструкции Python»/2.5_3.py
+++
@@ -1,9 +0,0 @@
-m = int(input())
-n = int(input())
-k = (n - m) % 10
-a = []
-while n <= m:
-    a.append(n)
-    n = n + k
-b = ', '.join(map(str, a))
-print(b)
--- 2.5. КР «Базовые конструкции Python»/2.5_5.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-m = -1000000000
-for i in range(n):
-    a = []
-    while (x := input()) != 'next':
-        a.append(int(x))
-    m = max((sum(a) / len(a)), m)
-print(f'{m:.2f}')
