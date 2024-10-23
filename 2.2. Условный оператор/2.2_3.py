d7c738d8331b0bf835da70f98ff2f413fca2e386
Кутляева Софья <skutlyaeva@gmail.com>
2024-10-23 23:30:44 +0300

    Delete 2.2. Условный оператор directory

--- 2.2. Условный оператор/2.2_3.py
+++
@@ -1,9 +0,0 @@
-s1 = int(input())
-s2 = int(input())
-s3 = int(input())
-if s1 > s2 and s1 > s3:
-    print("Петя")
-if s2 > s1 and s2 > s3:
-    print("Вася")
-if s3 > s1 and s3 > s2:
-    print("Толя")
