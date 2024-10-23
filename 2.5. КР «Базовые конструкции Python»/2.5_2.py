b89c350405adbcfa0c279592087745f0798a4446
skutlyaeva <skutlyaeva@gmail.com>
2024-10-23 23:28:59 +0300

    2.5

---
+++ 2.5. КР «Базовые конструкции Python»/2.5_2.py
@@ -0,0 +1,11 @@
+s = input()
+a = int(input())
+b = int(input())
+if 'sum' in s:
+    print(a + b)
+if 'sub' in s:
+    print(a - b)
+if 'mult' in s:
+    print(a * b)
+if 'div' in s:
+    print(a // b)
